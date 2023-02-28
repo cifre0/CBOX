eedbc365198262961a2d348a3cf475df6dfb7c56
Cifre0 <99344829+cifre0@users.noreply.github.com>
2023-02-28 11:45:57 +0100

    Update 4_26to4_27.sh

--- maj/4_26to4_27.sh
+++ maj/4_26to4_27.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+### FONCTION ###
 function gestion_erreur () {
     if [ "$?" -eq "0" ]; then printf "$1    \U2705\n\n"; else printf '\u2620 Failed \U2757\U2757\U2757\n'; exit 1; fi
 }
 
 function update_helm () {
     TEST_VERSION_HELM=$(printf "$VERSION_HELM\n3.5.0" | sort -V | head -n 1)
-    echo "check de la version 'HELM' ..."
-    if [ "$TEST_VERSION_HELM" -ne "3.5.0" ]; then echo "mise à jour de votre helm ..."; helm repo update &> /dev/null ; fi
+    printf "\U231B check de la version 'HELM' ... \n"
+    if [ "$TEST_VERSION_HELM" -ne "3.5.0" ]; then printf "\U231B mise à jour de votre helm ... \n"; helm repo update &> /dev/null ; fi
     gestion_erreur "mise à jour de HELM réussi"
 }
 
@@ -24,7 +25,7 @@ function scale_down () {
 }
 
 function update_CBOX () {
-    echo "Debut de la mise à jour ..."
+    printf "\U231B Debut de la mise à jour ... \n"
     if [[ ! -z "$var" ]]; 
         then
             helm upgrade -f myvalues.yml cryptobox --namespace $NAMESPACE cryptobox/cryptobox --version $VERSION_CBOX_SPECIFIQUE;
@@ -35,18 +36,19 @@ function update_CBOX () {
 }
 
 function scale_up () {
-    echo "scale-up des services ..."
+    printf "\U231B scale-up des services ... \n"
     kubectl scale deployment css css-periodic-tasks admin chap csws ctm --replicas=0 -n $NAMESPACE
     kubectl scale statefulset $NAMESPACE-redis-master $NAMESPACE-fluentd --replicas=0 -n $NAMESPACE
     gestion_erreur "scale-up réussi"
 }
 
-# Variable pour la fonction update_helm
+### VARIABLE ###
+# Pour la fonction update_helm
 VERSION_HELM = $(helm version | cut -d '"' -f2 | cut -d "v" -f2)
-# variable pour la fonction scale_down
+# Pour la fonction scale_down
 VERSION_CBOX = $(kubectl describe pod -n cryptobox | grep version | head -n 1 | cut -d"=" -f2)
 NAMESPACE = "cryptobox"
-# variable pour la fonction upadate_CBOX
+# Pour la fonction upadate_CBOX
 VERSION_CBOX_SPECIFIQUE = ""
 
 function main () {
